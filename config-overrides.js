896443176c318c329f00fb2a179b5d1a23685f1c
maro <honeymaro@live.co.kr>
2020-01-30 01:54:08 +0900

    Update: hot loader, fix bugs, etc

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+const rewireReactHotLoader = require("react-app-rewire-hot-loader-for-customize-cra");
+
 const { addDecoratorsLegacy, override } = require("customize-cra");
 
-module.exports = override(addDecoratorsLegacy());
+module.exports = override(rewireReactHotLoader(), addDecoratorsLegacy());
--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { hot } from "react-hot-loader/root";
 import { inject, observer } from "mobx-react";
 import { Helmet } from "react-helmet";
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+
+import Main from "pages/Main";
 
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
@@ -17,13 +21,15 @@ class App extends Component {
           <meta charSet="utf-8" />
           <title>app-name</title>
         </Helmet>
-        <Header />
+        {/* <Header /> */}
         <ToastContainer position={toast.POSITION.TOP_CENTER} />
-        <Switch>{/* <Route path='/' exact component={Main} /> */}</Switch>
-        <Footer />
+        <Switch>
+          <Route path="/" exact component={Main} />
+        </Switch>
+        {/* <Footer /> */}
       </div>
     );
   }
 }
 
-export default App;
+export default process.env.NODE_ENV === "development" ? hot(App) : App;
